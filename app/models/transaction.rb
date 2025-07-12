ab02e0d6670a3163662dcfdb8a4378dbcdd177c7
Andrey Yurov <a.yurov6@gmail.com>
2025-07-12 23:18:29 +0500

    Code refactor for Transaction model: added new lines, moved before_validation callback after scope section

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,11 +1,14 @@
 class Transaction < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
   validates :amount, presence: true
   validates :transaction_type, presence: true
-  before_validation :set_default_date, on: :create
+
   scope :income, -> { where(transaction_type: 'income') }
   scope :expense, -> { where(transaction_type: 'expense') }
+
+  before_validation :set_default_date, on: :create
 
   private
 
