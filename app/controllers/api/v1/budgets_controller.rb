c0b95eb9746bd71e7dab99929be8155f5e7b0e2a
Andrey Yurov <a.yurov6@gmail.com>
2025-07-12 20:50:57 +0500

    Implemented budget create endpoint

--- app/controllers/api/v1/budgets_controller.rb
+++ app/controllers/api/v1/budgets_controller.rb
@@ -5,6 +5,15 @@ class Api::V1::BudgetsController < ApplicationController
   end
 
   def create
-    render json: { message: "Creates budget endpoint" }
+    budget = current_user.budgets.build(budget_params)
+    if budget.save
+      render json: budget, status: :created
+    else
+      render json: { errors: budget.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def budget_params
+    params.require(:budget).permit(:monthly_limit, :category_id, :month)
   end
 end
