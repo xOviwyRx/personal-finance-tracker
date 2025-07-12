be9ecde021f647e169f119fcb59c8fd8cbfcaafe
Andrey Yurov <a.yurov6@gmail.com>
2025-07-13 03:46:35 +0500

    Added tests to Budget model

--- spec/models/budget_spec.rb
+++ spec/models/budget_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Budget, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has monthly_limit' do
+    budget = Budget.new(monthly_limit: 10)
+    expect(budget.monthly_limit).to eq(10)
+  end
+
+  it 'is invalid without monthly_limit' do
+    budget = Budget.new(monthly_limit: nil)
+    expect(budget).not_to be_valid
+  end
+
+  it 'belongs to user' do
+    expect(Budget.reflect_on_association(:user).macro).to eq(:belongs_to)
+  end
+
+  it 'belongs to category' do
+    expect(Budget.reflect_on_association(:category).macro).to eq(:belongs_to)
+  end
 end
