743597177cd34f0b99ce15339b4241ff5ba5d386
Andrey Yurov <a.yurov6@gmail.com>
2025-07-12 23:15:54 +0500

    Added transaction_type presence validation to Transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,6 +2,7 @@ class Transaction < ApplicationRecord
   belongs_to :user
   belongs_to :category
   validates :amount, presence: true
+  validates :transaction_type, presence: true
   before_validation :set_default_date, on: :create
   scope :income, -> { where(transaction_type: 'income') }
   scope :expense, -> { where(transaction_type: 'expense') }
