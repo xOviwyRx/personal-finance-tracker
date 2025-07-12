bb7e8489a29ce0ef2bb0c9c4fa28e12fed3a48ad
Andrey Yurov <a.yurov6@gmail.com>
2025-07-13 02:04:24 +0500

    Added tests for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Test basic Devise integration
+  it 'is valid with email and password' do
+    user = User.new(email: 'text@example.com', password: 'password')
+    expect(user).to be_valid
+  end
+
+  it 'is invalid without email' do
+    user = User.new(password: 'password')
+    expect(user).not_to be_valid
+  end
+
+  it 'duplicate email' do
+    User.create(email: 'test@example.com', password: 'password')
+    user2 = User.new(email: 'test@example.com', password: 'password')
+    expect(user2).not_to be_valid
+  end
+
+  it 'is invalid without password' do
+    user = User.new(email:'text@example.com')
+    expect(user).not_to be_valid
+  end
+
+  # Test associations
+  it 'has many categories' do
+    expect(User.reflect_on_association(:categories).macro).to eq(:has_many)
+  end
+
+  it 'has many budgets' do
+    expect(User.reflect_on_association(:budgets).macro).to eq(:has_many)
+  end
+
+  it 'has many transactions' do
+    expect(User.reflect_on_association(:transactions).macro).to eq(:has_many)
+  end
 end
