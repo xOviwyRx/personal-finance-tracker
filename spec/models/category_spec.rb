d716cf1fa0b9a2a0bf30575276b0d5cd69eb2758
Andrey Yurov <a.yurov6@gmail.com>
2025-07-13 02:31:35 +0500

    Added tests for category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has name' do
+    category = Category.new(name: "Category Name")
+    expect(category.name).to eq("Category Name")
+  end
+
+  it 'is invalid without name' do
+    category = Category.new
+    expect(category).not_to be_valid
+  end
+
+  it 'has many transactions' do
+    expect(Category.reflect_on_association(:transactions).macro).to eq(:has_many)
+  end
+
+  it 'belongs to user' do
+    expect(Category.reflect_on_association(:user).macro).to eq(:belongs_to)
+  end
+
+  it 'unique name per user' do
+    user = User.create(email: 'test@example.com', password: 'password')
+    Category.create(name: 'Food', user: user)
+
+    duplicate_category = Category.new(name: 'Food', user: user)
+    expect(duplicate_category).not_to be_valid
+  end
+
+  it 'permit create same name with different user' do
+    user1 = User.create(email: 'test@example.com', password: 'password')
+    user2 = User.create(email: 'test2@example.com', password: 'password')
+
+    Category.create(name: 'Food', user: user1)
+    category2 = Category.new(name: 'Food', user: user2)
+    expect(category2).to be_valid
+  end
 end
