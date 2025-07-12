be6db68784bcbd51bf68dd56fac7d4740033df9d
Andrey Yurov <a.yurov6@gmail.com>
2025-07-12 23:06:41 +0500

    Added default date for transactions

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,6 +2,13 @@ class Transaction < ApplicationRecord
   belongs_to :user
   belongs_to :category
   validates :amount, presence: true
+  before_validation :set_default_date, on: :create
   scope :income, -> { where(transaction_type: 'income') }
   scope :expense, -> { where(transaction_type: 'expense') }
+
+  private
+
+  def set_default_date
+    self.date ||= Date.current
+  end
 end
