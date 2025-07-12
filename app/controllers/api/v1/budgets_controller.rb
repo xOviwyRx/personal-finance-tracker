6680c370ce289bd21fbf9341bcb1d9d78b10b067
Andrey Yurov <a.yurov6@gmail.com>
2025-07-12 20:59:50 +0500

    Implemented budget update endpoint

--- app/controllers/api/v1/budgets_controller.rb
+++ app/controllers/api/v1/budgets_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::BudgetsController < ApplicationController
+  before_action :set_budget, only: [:update]
   def index
     budgets = current_user.budgets.includes(:category)
     render json: budgets.as_json(include: {category: { only: [:id, :name] } })
@@ -11,6 +12,18 @@ class Api::V1::BudgetsController < ApplicationController
     else
       render json: { errors: budget.errors.full_messages }, status: :unprocessable_entity
     end
+  end
+
+  def update
+    if @budget.update(budget_params)
+      render json: @budget
+    else
+      render json: { errors: @budget.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def set_budget
+    @budget = current_user.budgets.find(params[:id])
   end
 
   def budget_params
