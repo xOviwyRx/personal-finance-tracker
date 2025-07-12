fc99e1c4bf6e681f0a45f2b89d57009d54d04865
Andrey Yurov <a.yurov6@gmail.com>
2025-07-12 18:24:38 +0500

    Updated Category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ApplicationRecord
+    belongs_to :user
     has_many :transactions
+
+    validates :name, presence: true
+    validates :name, uniqueness: { scope: :user_id }
 end
