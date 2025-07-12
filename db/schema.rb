e8eaa47e8f3d05bb017a34fdffb3061be44f72ae
Andrey Yurov <a.yurov6@gmail.com>
2025-07-12 20:27:37 +0500

    Added user_id to budget table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_12_133333) do
+ActiveRecord::Schema[7.1].define(version: 2025_07_12_152452) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,7 +20,9 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_12_133333) do
     t.bigint "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["category_id"], name: "index_budgets_on_category_id"
+    t.index ["user_id"], name: "index_budgets_on_user_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -55,6 +57,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_12_133333) do
   end
 
   add_foreign_key "budgets", "categories"
+  add_foreign_key "budgets", "users"
   add_foreign_key "categories", "users"
   add_foreign_key "transactions", "categories"
 end
