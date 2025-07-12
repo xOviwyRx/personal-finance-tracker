a55f34bea8bae2c374b7aebf9bcfda8714473c99
Andrey Yurov <a.yurov6@gmail.com>
2025-07-12 19:01:47 +0500

    Implemented create category endpoint

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -4,6 +4,15 @@ class Api::V1::CategoriesController < ApplicationController
   end
 
   def create
-    render json: { message: "Create category endpoint" }
+    category = current_user.categories.build(category_params)
+    if category.save
+      render json: category, status: :created
+    else
+      render json: { errors:category.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
