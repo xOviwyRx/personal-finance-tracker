a344df374ce7acab41023f20a33100b763356bb8
Andrey Yurov <a.yurov6@gmail.com>
2025-07-12 18:35:18 +0500

    Added user_id to Category table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_10_204713) do
+ActiveRecord::Schema[7.1].define(version: 2025_07_12_133333) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_10_204713) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
   create_table "transactions", force: :cascade do |t|
@@ -53,5 +55,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_10_204713) do
   end
 
   add_foreign_key "budgets", "categories"
+  add_foreign_key "categories", "users"
   add_foreign_key "transactions", "categories"
 end
