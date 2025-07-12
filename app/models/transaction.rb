e9e372421d8754ce2c95a24333f79b0501b7bb1f
Andrey Yurov <a.yurov6@gmail.com>
2025-07-12 23:28:46 +0500

    Added inclusion validate for transaction_type of Transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,7 +3,7 @@ class Transaction < ApplicationRecord
   belongs_to :category
 
   validates :amount, presence: true
-  validates :transaction_type, presence: true
+  validates :transaction_type, presence: true, inclusion: { in: %w[income expense] }
 
   scope :income, -> { where(transaction_type: 'income') }
   scope :expense, -> { where(transaction_type: 'expense') }
