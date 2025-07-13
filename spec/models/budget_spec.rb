407175948c5bdf3d44b94ee602b7342e8e17b5aa
Andrey Yurov <a.yurov6@gmail.com>
2025-07-13 15:22:21 +0500

    Updated Budget model test: is invalid without monthly limit

--- spec/models/budget_spec.rb
+++ spec/models/budget_spec.rb
@@ -7,8 +7,12 @@ RSpec.describe Budget, type: :model do
   end
 
   it 'is invalid without monthly_limit' do
-    budget = Budget.new(monthly_limit: nil)
+    user = User.create(email: 'test@example.com', password: 'password123')
+    category = Category.create(name: 'Food', user: user)
+
+    budget = Budget.new(monthly_limit: nil, user: user, category: category)
     expect(budget).not_to be_valid
+    expect(budget.errors[:monthly_limit]).to include("can't be blank")
   end
 
   it 'belongs to user' do
