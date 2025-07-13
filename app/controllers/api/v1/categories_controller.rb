8d740e84cd14d28391a9befc780df6b671a4813b
Andrey Yurov <a.yurov6@gmail.com>
2025-07-13 22:24:12 +0500

    Code refactor: in create method use instance variable for consistency

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -12,11 +12,11 @@ class Api::V1::CategoriesController < ApplicationController
   end
 
   def create
-    category = current_user.categories.build(category_params)
-    if category.save
-      render json: category, status: :created
+    @category = current_user.categories.build(category_params)
+    if @category.save
+      render json: @category, status: :created
     else
-      render json: { errors:category.errors.full_messages }, status: :unprocessable_entity
+      render json: { errors:@category.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
