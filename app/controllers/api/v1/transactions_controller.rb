3ec5567e06d8f4d9ad33cb89b4a1767f73d9f292
Andrey Yurov <a.yurov6@gmail.com>
2025-07-14 17:37:07 +0500

    Added search for transactions and ordered them by descendent order

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::TransactionsController < ApplicationController
   load_and_authorize_resource
   def index
+    @q = @transactions.ransack(params[:q])
+    @transactions = @q.result.order(created_at: :desc)
+
     render json: @transactions
   end
 
