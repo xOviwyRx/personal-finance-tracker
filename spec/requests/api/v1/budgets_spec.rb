1548eafd62654c50a8049cd74935890ba405b039
Andrey Yurov <a.yurov6@gmail.com>
2025-07-14 15:45:56 +0500

    Added authentication tests for budget requests, moved authentication before to 'when authnticated' contexts

--- spec/requests/api/v1/budgets_spec.rb
+++ spec/requests/api/v1/budgets_spec.rb
@@ -15,59 +15,83 @@ RSpec.describe "Api::V1::Budgets", type: :request do
     category: category,
     month: '2023-02-01'
   ) }
-  before do
-    post '/api/v1/users/sign_in', params: {
-      user: { email: 'test@example.com', password: 'password' }
-    }, as: :json
-  end
 
   describe "GET /api/v1/budgets" do
-
-    it 'returns status code 200 and budgets' do
+    it 'returns 401 when not authenticated' do
       get '/api/v1/budgets'
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:unauthorized)
     end
 
-    it 'returns budgets' do
-      get '/api/v1/budgets'
+    context 'when authenticated' do
+      before do
+        post '/api/v1/users/sign_in', params: {
+          user: { email: 'test@example.com', password: 'password' }
+        }, as: :json
+      end
 
-      json_response = JSON.parse(response.body)
-      expect(json_response.length).to eq(2)
+      it 'returns status code 200 and budgets' do
+        get '/api/v1/budgets'
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'returns budgets' do
+        get '/api/v1/budgets'
+
+        json_response = JSON.parse(response.body)
+        expect(json_response.length).to eq(2)
+      end
     end
 
   end
 
   describe "POST /api/v1/budgets" do
-    it 'returns status code 201 and created budget' do
+    it 'returns 401 when not authenticated' do
       post '/api/v1/budgets', params: {
         budget: {
           category_id: category.id,
           monthly_limit: '3000.0',
-          month: '2023-01-01'
+          month: '2023-03-01'
         }
       }
-      expect(response).to  have_http_status(:created)
-      json_response = JSON.parse(response.body)
-      expect(json_response['monthly_limit']).to eq('3000.0')
-      expect(json_response['month']).to eq('2023-01-01')
-      expect(json_response).to have_key('id')
-      expect(json_response).to have_key('created_at')
-      expect(json_response['category_id']).to eq(category.id)
+      expect(response).to have_http_status(:unauthorized)
     end
 
-    it 'returns status code 422 when category is missing' do
-      post '/api/v1/budgets', params: {
-        budget: {
-          monthly_limit: '3000.0',
-          month: '2023-01-01'
+    context 'when authenticated' do
+      before do
+        post '/api/v1/users/sign_in', params: {
+          user: { email: 'test@example.com', password: 'password' }
+        }, as: :json
+      end
+
+      it 'returns status code 201 and created budget' do
+        post '/api/v1/budgets', params: {
+          budget: {
+            category_id: category.id,
+            monthly_limit: '3000.0',
+            month: '2023-01-01'
+          }
         }
-      }
+        expect(response).to  have_http_status(:created)
+        json_response = JSON.parse(response.body)
+        expect(json_response['monthly_limit']).to eq('3000.0')
+        expect(json_response['month']).to eq('2023-01-01')
+        expect(json_response).to have_key('id')
+        expect(json_response).to have_key('created_at')
+        expect(json_response['category_id']).to eq(category.id)
+      end
 
-      expect(response).to have_http_status(:unprocessable_entity)
-      json_response = JSON.parse(response.body)
-      expect(json_response['errors']).to include("Category must exist")
+      it 'returns status code 422 when category is missing' do
+        post '/api/v1/budgets', params: {
+          budget: {
+            monthly_limit: '3000.0',
+            month: '2023-01-01'
+          }
+        }
+
+        expect(response).to have_http_status(:unprocessable_entity)
+        json_response = JSON.parse(response.body)
+        expect(json_response['errors']).to include("Category must exist")
+      end
     end
-
-
   end
 end
