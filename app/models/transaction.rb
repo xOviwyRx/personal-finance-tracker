d0de0a59d9b18289049c9596c8d69fccfe0ed64e
Andrey Yurov <a.yurov6@gmail.com>
2025-07-15 23:10:32 +0500

    Add custom validation for transaction model: users can create transactions with their categories only

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -8,10 +8,18 @@ class Transaction < ApplicationRecord
 
   before_validation :set_default_date, on: :create
 
+  validate :category_belongs_to_user
+
   private
 
   def set_default_date
     self.date ||= Date.current
+  end
+
+  def category_belongs_to_user
+    if category&.user != user
+      errors.add(:category, "must belong to the same user")
+    end
   end
 
   def self.ransackable_attributes(auth_object = nil)
