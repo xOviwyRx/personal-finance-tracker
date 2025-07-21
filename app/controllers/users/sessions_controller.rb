c15defd60859fbf2be3ffe4d60c1639ecfd4f043
Andrey Yurov <a.yurov6@gmail.com>
2025-07-22 02:55:55 +0500

    Auth: update session controller according to JWT token based authentication

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,7 +3,22 @@
 class Users::SessionsController < Devise::SessionsController
   respond_to :json
 
+  def create
+    user = User.find_for_database_authentication(email: sign_in_params[:email])
+
+    if user&.valid_password?(sign_in_params[:password])
+      self.resource = user
+      sign_in(resource_name, resource)
+      respond_with(resource)
+    else
+      render json: { error: 'Invalid email or password' }, status: :unauthorized
+    end
+  end
+
   private
+  def sign_in_params
+    params.require(:user).permit(:email, :password)
+  end
 
   def respond_with(resource, _opts = {})
     render json: {
