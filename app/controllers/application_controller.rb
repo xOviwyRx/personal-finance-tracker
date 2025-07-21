77fe80865ab19b86bd76eead7eecfc11cc6e241b
Andrey Yurov <a.yurov6@gmail.com>
2025-07-22 02:21:21 +0500

    Auth: Update application controller according to new auth routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::API
-    before_action :authenticate_user!
+    before_action :authenticate_api_v1_user!
     include CanCan::ControllerAdditions
     rescue_from CanCan::AccessDenied do |exception|
         render json: { error: 'Access denied' }, status: :forbidden
+    end
+
+    private
+    def current_user
+        current_api_v1_user
+    end
+
+    def current_ability
+        @current_ability ||= ::Ability.new(current_user)
     end
 end
