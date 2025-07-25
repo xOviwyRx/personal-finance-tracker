6434597dff2f5bc80ab892ac1cf3d900c8441dc8
Andrey Yurov <a.yurov6@gmail.com>
2025-07-26 01:07:04 +0500

    JWT Auth: configure routes and application controller for jwt auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,36 @@
 class ApplicationController < ActionController::API
-    before_action :authenticate_api_v1_user!
+    before_action :authenticate_user!
     include CanCan::ControllerAdditions
     rescue_from CanCan::AccessDenied do |exception|
         render json: { error: 'Access denied' }, status: :forbidden
     end
 
     private
+
+    def authenticate_user!
+        token = token_from_request
+        return render json: { error: 'Missing token' }, status: :unauthorized unless token
+
+        begin
+            decoded_token = Warden::JWTAuth::TokenDecoder.new.call(token)
+            user_id = decoded_token['sub']
+            @current_user = User.find(user_id)
+        rescue JWT::DecodeError, ActiveRecord::RecordNotFound
+            render json: { error: 'Invalid token' }, status: :unauthorized
+        end
+    end
+
     def current_user
-        current_api_v1_user
+        @current_user
     end
 
     def current_ability
         @current_ability ||= ::Ability.new(current_user)
+    end
+
+    def token_from_request
+        pattern = /^Bearer /
+        header = request.headers['Authorization']
+        header.gsub(pattern, '') if header&.match(pattern)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,11 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      devise_for :users, defaults: { format: :json }, controllers: {
-        sessions: 'users/sessions',
-        registrations: 'users/registrations'
-      }
+
+      post   'users/sign_in',  to: 'auth#login'
+      post   'users',          to: 'auth#signup'
+      delete 'users/sign_out', to: 'auth#logout'
+
       resources :transactions
       resources :budgets
       resources :categories
