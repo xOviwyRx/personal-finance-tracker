14e3824abb3841cb612306dada9adcd0249075e7
Andrey Yurov <a.yurov6@gmail.com>
2025-07-26 01:03:31 +0500

    JWT Auth: replace registrations_controllers and sessions_controllers by auth_controller

---
+++ app/controllers/api/v1/auth_controller.rb
@@ -0,0 +1,49 @@
+class Api::V1::AuthController < ApplicationController
+  skip_before_action :authenticate_user!
+  def login
+    user = User.find_by(email: params[:user][:email])
+
+
+    if user&.valid_password?(params[:user][:password])
+      token, _payload = Warden::JWTAuth::UserEncoder.new.call(user, :user, nil)
+      render json: {
+        token: token,
+        user: {
+          id: user.id,
+          email: user.email
+        }
+      }
+    else
+      render json: { error: "Invalid credentials" }, status: :unauthorized
+    end
+  end
+
+  def signup
+    user = User.new(user_params)
+
+    if user.save
+      token, *payload = Warden::JWTAuth::UserEncoder.new.call(user, :user, nil)
+      render json: {
+        token: token,
+        user: {
+          id: user.id,
+          email: user.email
+        }
+      }, status: :created
+    else
+      render json: {
+        errors: user.errors.full_messages
+      }, status: :unprocessable_entity
+    end
+  end
+
+  def logout
+    render json: { message: 'Logged out successfully' }
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
