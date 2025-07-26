289304fb59e26ad325e16a31bf317e4b508152b5
Andrey Yurov <a.yurov6@gmail.com>
2025-07-26 17:13:30 +0500

    Validation: add uniqueness for budget per user per category per month

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -2,6 +2,8 @@ class Budget < ApplicationRecord
   belongs_to :category
   belongs_to :user
   validates :monthly_limit, presence: true
+  validates :user_id, uniqueness: { scope: [:category_id, :month],
+                                    message: "Budget already exists for this category and month" }
 
   def self.ransackable_attributes(auth_object = nil)
     %w[category_id month]
