68f989e1aa1db790fecd19a1627b80b9f1d65f9f
Andrey Yurov <a.yurov@voiso.com>
2026-04-27 13:43:09 +0400

    Allow Railway hosts in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,6 +97,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.hosts << "personal-finance-tracker-production-bd6c.up.railway.app"
+
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
