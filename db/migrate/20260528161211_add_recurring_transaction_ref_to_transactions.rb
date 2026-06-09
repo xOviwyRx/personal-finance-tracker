87da2f0f552980b78f6b25b29530a1789b36c6bd
Andrey Yurov <a.yurov6@gmail.com>
2026-06-09 13:19:21 +0400

    Rubocop fix

--- db/migrate/20260528161211_add_recurring_transaction_ref_to_transactions.rb
+++ db/migrate/20260528161211_add_recurring_transaction_ref_to_transactions.rb
@@ -1,7 +1,7 @@
 class AddRecurringTransactionRefToTransactions < ActiveRecord::Migration[7.1]
   def change
     add_reference :transactions, :recurring_transaction, null: true, index: false,
-                  foreign_key: { on_delete: :nullify }
+                                                         foreign_key: { on_delete: :nullify }
     add_index :transactions, [:recurring_transaction_id, :date],
               unique: true, where: "recurring_transaction_id IS NOT NULL",
               name: "index_transactions_on_recurring_rule_and_date"
